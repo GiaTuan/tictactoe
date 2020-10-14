862c7a4d7356387d96c2de2aa506b6abb6bf1ebb
tuan <adgtuan@gmail.com>
2020-10-14 09:32:23 +0700

    Updates

---
+++ precache-manifest.a5334f24d0c5740d065a959afcad6bd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0d746c44c180a5100dff6d831d04525",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "61c1337ba8707265074e",
+    "url": "/tictactoe/static/css/main.1e5c2b1d.chunk.css"
+  },
+  {
+    "revision": "91e02a7922052f6505c7",
+    "url": "/tictactoe/static/js/2.f0fcb066.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe/static/js/2.f0fcb066.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61c1337ba8707265074e",
+    "url": "/tictactoe/static/js/main.d4cb4dc2.chunk.js"
+  },
+  {
+    "revision": "fafba3c6dec195f2ac0d",
+    "url": "/tictactoe/static/js/runtime-main.1cc0721e.js"
+  }
+]);
